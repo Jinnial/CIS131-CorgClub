e4eca06dd84867ae0e3063adc7fe36f28896d7a7
Jordan Gilmore <gilmorej@otc.edu>
2017-09-15 23:25:46 -0500

    Add files via upload
    Okay here is the HTML, CSS, .js, and images for my page. The functions work fine. We may want to do something fancier. The page colors... I've stared at it too long. Can't tell if it looks like vomit or not. I am aware the rows have way to much blank space in them. Suggests?

---
+++ adoptee.js
@@ -0,0 +1,86 @@
+// Developer: Jordan Gilmore
+// Date:      9/15/17
+// FileName:  adoptee.js
+
+var topCorgs = ["images/topCorg0.jpg", "images/topCorg1.jpg",
+                "images/topCorg2.jpg","images/topCorg3.jpg"];
+var midCorgs = ["images/midCorg0.jpg", "images/midCorg1.jpg",
+                "images/midCorg2.jpg","images/midCorg3.jpg"];
+var bottomCorgs = ["images/bottomCorg0.jpg", "images/bottomCorg1.jpg",
+                "images/bottomCorg2.jpg","images/bottomCorg3.jpg"];
+
+var theTopSwitcher = 1;
+var theMidSwitcher = 1;
+var theBottomSwitcher = 1;
+
+function festCorg()
+{
+  var newSrc;
+  newSrc = "images/corgGif.gif";
+  document.getElementById('festGif').src = newSrc;
+}
+function toggleTopCorgi()
+{
+  switch (theTopSwitcher)
+  {
+    case 0:
+      document.getElementById('corgLeft1').src = topCorgs[theTopSwitcher];
+      theTopSwitcher = 1;
+      break;
+    case 1:
+      document.getElementById('corgLeft1').src = topCorgs[theTopSwitcher];
+      theTopSwitcher = 2;
+      break;
+    case 2:
+      document.getElementById('corgLeft1').src = topCorgs[theTopSwitcher];
+      theTopSwitcher = 3;
+      break;
+    default:
+      document.getElementById('corgLeft1').src = topCorgs[theTopSwitcher];
+      theTopSwitcher = 0;
+  }
+}
+
+function toggleMidCorgi()
+{
+  switch (theMidSwitcher)
+  {
+    case 0:
+      document.getElementById('corgRight1').src = midCorgs[theMidSwitcher];
+      theMidSwitcher = 1;
+      break;
+    case 1:
+      document.getElementById('corgRight1').src = midCorgs[theMidSwitcher];
+      theMidSwitcher = 2;
+      break;
+    case 2:
+      document.getElementById('corgRight1').src = midCorgs[theMidSwitcher];
+      theMidSwitcher = 3;
+      break;
+    default:
+      document.getElementById('corgRight1').src = midCorgs[theMidSwitcher];
+      theMidSwitcher = 0;
+  }
+}
+
+function toggleBottomCorgi()
+{
+  switch (theBottomSwitcher)
+  {
+    case 0:
+      document.getElementById('corgLeft2').src = bottomCorgs[theBottomSwitcher];
+      theBottomSwitcher = 1;
+      break;
+    case 1:
+      document.getElementById('corgLeft2').src = bottomCorgs[theBottomSwitcher];
+      theBottomSwitcher = 2;
+      break;
+    case 2:
+      document.getElementById('corgLeft2').src = bottomCorgs[theBottomSwitcher];
+      theBottomSwitcher = 3;
+      break;
+    default:
+      document.getElementById('corgLeft2').src = bottomCorgs[theBottomSwitcher];
+      theBottomSwitcher = 0;
+  }
+}
